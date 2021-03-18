f45265fe405396442a7b8fd5424648bac80b6b0e
Cristina Golban <cristina.golban@amdaris.com>
2021-03-18 14:47:27 +0200

    conflict1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,12 +9,10 @@ namespace ConsoleApp2
             Console.WriteLine("Salut Main!");
             Console.WriteLine("Salut brench 1 !");
 
-<<<<<<< HEAD
             Console.WriteLine("Alta scgimbare2 de mesaj in brench1 :)")
 
-=======
-            Console.WriteLine("Alta sss de mesaj in brench1 :)")
->>>>>>> brench1
+            Console.WriteLine("Alta sss de mesaj in brench1 vvvvvv :)")
+
         }
     }
 }
